df12bf2614a0ea047c0f9ecea0d62ae9fe665801
Segger87 <Segger87@gmail.com>
2018-06-11 10:45:59 +0100

    Implemented a ToString Override Method

---
+++ DuckDictionary/DuckDictionary/Duck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckDictionary
+{
+	public class Duck
+	{
+		public string Name { get; }
+		public string Type { get; }
+		public int WeightInGrams { get; }
+		public int AgeInMonths { get; }
+
+		public Duck(string name, string type, int weightInGrams, int ageInMonths)
+		{
+			Name = name;
+			Type = type;
+			WeightInGrams = weightInGrams;
+			AgeInMonths = ageInMonths;
+		}
+		public override string ToString()
+		{
+			return $"Name = {Name}, Type = {Type} Weight in Grams = {WeightInGrams} Age in Months = {AgeInMonths}";
+		}
+	}
+
+}
---
+++ DuckDictionary/DuckDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckDictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var barry = new Duck("Barry", "Pond Duck", 20, 599);
+
+			Console.WriteLine(barry.ToString());
+			Console.ReadLine();
+		}
+	}
+
+
+}
